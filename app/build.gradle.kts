fa2c0f8baba1693577e2280b8e54739b296e4202
hjh <211787334@qq.com>
2026-05-14 16:23:49 +0800

    Add release signing config + gitignore update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,9 +20,19 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("release.jks")
+            storePassword = "123456"
+            keyAlias = "VinylShop"
+            keyPassword = "123456"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
